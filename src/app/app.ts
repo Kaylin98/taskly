accde4971efecf1a5b8ce0620556b2374f003413
Kaylin Maharaj <kaylinmaharaj@gmail.com>
2026-07-14 17:37:49 +0200

    feat: add tasks component and integrate user selection functionality

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header";
 import { User } from "./user/user";
 import { DUMMY_USERS } from './dummy-users';
+import { Tasks } from './tasks/tasks';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, User],
+  imports: [Header, User, Tasks],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -14,10 +14,14 @@ export class App {
   protected readonly title = signal('taskly');
 
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
 
+  get selectedUser() {
+    return this.users.find(u => u.id === this.selectedUserId)!;
+  }
 
   onUserSelected(Id: string) {
-    console.log(`User selected: ${Id}`);
+    this.selectedUserId = Id;
   }
 
 }
